c2c01f8768efd770ac77c1ed00dcb866a24244e2
Junyup Hong <junyup.hong@gmail.com>
2018-08-10 14:30:28 +0900

    Promise.all() 사용한 example

--- public/javascripts/asyncExample.js
+++ public/javascripts/asyncExample.js
@@ -1,3 +1,5 @@
+//get(), getJSON() 은 언제 불러올지 모른다...(순서가 뒤죽박죽)
+
 // const a = ['heo', 'hong', 'gong', 'lee'];
 //
 // for(let j = 0; j <3 ; j++) {
@@ -11,9 +13,88 @@
 
 
 
+/**
+ * without promise
+ */
+
+//
+// const selected = [];
+// const jsonArr = [];
+//
+// const $person = $('.person');
+// const $button = $('.button');
+// const $textZone = $('.text-zone');
+// const $resultZone = $('.result-zone');
+//
+// let count = 0;
+// let func;
+//
+// const getJson = function (url) {
+//     $.getJSON(url, function (json) {
+//         if(count < selected.length){
+//             console.log('push','next', json);
+//             pushJson(json, 'getJson');
+//         }
+//         else {
+//             console.log('push', json);
+//             pushJson(json);
+//
+//         }
+//     });
+//     count++;
+// };
+//
+// const pushJson = function (json, next) {
+//     jsonArr.push(json);
+//     if(next ==='getJson') {
+//         getJson(`/data/${selected[count]}.json`);
+//     }
+//     else if(next === undefined){
+//         appendJson();
+//     }
+// };
+//
+//
+//
+//
+// const appendJson = () => {
+//     $resultZone.empty();
+//
+//     for(let i = 0; i< jsonArr.length; i++) {
+//         $resultZone.append(`<div class="result">{name: "${jsonArr[i].name}", age: ${jsonArr[i].age}}</div>`);
+//     }
+//
+// };
+//
+//
+// $person.on('click', function() {
+//     $(this).toggleClass("background");
+// });
+//
+// $button.on('click', function () {
+//     selected.length = 0;
+//     jsonArr.length = 0;
+//     count = 0;
+//
+//
+//     const a = $textZone.find('.background');
+//     for(let i = 0; i< a.length; i++) {
+//         selected.push(a[i].id);
+//     }
+//
+//     if(selected.length >0) {
+//         getJson(`/data/${selected[count]}.json`);
+//     }
+//
+//
+// });
 
 
 
+
+/**
+ *  promise 사용
+ **/
 
 // const selected = [];
 // const jsonArr = [];
@@ -29,10 +110,13 @@
 //
 // const getJson = (url) => {
 //     return new Promise((resolve, reject) => {
-//         $.getJSON(url, function (json) {
-//             resolve(json);
-//
-//         });
+//         try {
+//             $.getJSON(url, function (json) {
+//                 resolve(json);
+//             });
+//         }catch(e) {
+//             reject(e);
+//         }
 //     } );
 // };
 //
@@ -67,82 +151,68 @@
 //
 //     appendJson();
 // });
-
-
+//
 
 
 /**
- * without promise
+ *  Promise.all() 사용
  */
 
-
 const selected = [];
-const jsonArr = [];
+const promises = [];
+let json;
+
 
 const $person = $('.person');
 const $button = $('.button');
 const $textZone = $('.text-zone');
 const $resultZone = $('.result-zone');
 
-let count = 0;
-let func;
 
-const getJson = function (url) {
-    $.getJSON(url, function (json) {
-        if(count < selected.length){
-            console.log('push','next', json);
-            pushJson(json, 'getJson');
+
+
+const getJson = (url) => {
+    return new Promise((resolve, reject) => {
+        try {
+            $.getJSON(url, function (json) {
+                resolve(json);
+            });
+        }catch(e) {
+            reject(e);
         }
-        else {
-            console.log('push', json);
-            pushJson(json);
-
-        }
-    });
-    count++;
+    } );
 };
-
-const pushJson = function (json, next) {
-    jsonArr.push(json);
-    if(next ==='getJson') {
-        getJson(`/data/${selected[count]}.json`);
-    }
-    else if(next === undefined){
-        appendJson();
-    }
-};
-
-
-
-
 const appendJson = () => {
     $resultZone.empty();
-
-    for(let i = 0; i< jsonArr.length; i++) {
-        $resultZone.append(`<div class="result">{name: "${jsonArr[i].name}", age: ${jsonArr[i].age}}</div>`);
+    for(let i = 0; i< json.length; i++) {
+        $resultZone.append(`<div class="result">{name: "${json[i].name}", age: ${json[i].age}}</div>`);
     }
-
 };
-
 
 $person.on('click', function() {
     $(this).toggleClass("background");
 });
 
-$button.on('click', function () {
+$button.on('click', async function () {
     selected.length = 0;
-    jsonArr.length = 0;
-    count = 0;
-
 
     const a = $textZone.find('.background');
     for(let i = 0; i< a.length; i++) {
         selected.push(a[i].id);
     }
+    //
+    // promises.length = 0;
+    // for (let i = 0; i < selected.length; i++) {
+    //     promises.push(getJson(`/data/${selected[i]}.json`));
+    // }
+    // json = await Promise.all(promises);
 
-    if(selected.length >0) {
-        getJson(`/data/${selected[count]}.json`);
-    }
+    json = await Promise.all(_.map(selected,
+        (s) => getJson(`/data/${s}.json`)));
 
 
+
+
+    appendJson();
 });
+
