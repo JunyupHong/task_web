d4795ba016d96f50b5d692a06458f7cdb6f2befa
Junyup Hong <junyup.hong@gmail.com>
2018-08-10 14:30:58 +0900

    babel

---
+++ src/babelExample.js
@@ -0,0 +1,18 @@
+function Element() {}
+const ele = new Element();
+
+function wait(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), ms);
+    })
+}
+
+async function t() {
+    await wait(1000);
+}
+
+t();
+
+const a = 0;
+
+const b = 0;
